066a760d7bc6ce7a568e4e2c00293da1892a1b63
MemoAllighieri <guillermoburga17@gmail.com>
2023-03-02 10:51:28 -0500

    modify  missionvision component

--- src/app/main/home/missionvision/missionvision.component.ts
+++ src/app/main/home/missionvision/missionvision.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./missionvision.component.css']
 })
 export class MissionvisionComponent {
+  ViewMissionVission: boolean = true;
 
+  ChangeViewMissionVission(tipo:boolean){
+    this.ViewMissionVission = tipo;
+  }
 }
