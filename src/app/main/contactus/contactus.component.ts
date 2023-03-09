663ea9abfd89226e647bccce4962c5632e181710
MemoAllighieri <guillermoburga17@gmail.com>
2023-03-09 16:31:39 -0500

    contact us edit

--- src/app/main/contactus/contactus.component.ts
+++ src/app/main/contactus/contactus.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import '../../../utils/smtp.js'; //path might change
+declare let Email : any;
+
 @Component({
   selector: 'app-contactus',
   templateUrl: './contactus.component.html',
@@ -14,8 +17,39 @@ export class ContactusComponent {
               {id: 5, value : "Aplicación móvil"},
               {id: 6, value : "Desarrollo web"}];
 
+  
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  SendEmail(){
+    Email.send({
+      Host : 'smtp.elasticemail.com',
+      Username : 'memoallighieri17@gmail.com',
+      Password : '708F13FA4D0DDDFF23D1AD548B51A04CDBD1',
+      To : 'andre_0828@outlook.com',
+      From : `memoallighieri17@gmail.com`,
+      Subject : 'prueba',
+      Body : `aaaaaaaaaaaaaaaaaaa`
+    }).then(
+      //message => alert(message)
+      console.log("ok")
+    );
+  }
+
+  // onSubmit(contactForm: NgForm) {
+  //   if (contactForm.valid) {
+  //     const email = contactForm.value;
+  //     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  //     this.http.post('https://formspree.io/asdlf7asdf',
+  //       { name: email.name, replyto: email.email, message: email.messages },
+  //       { 'headers': headers }).subscribe(
+  //         response => {
+  //           console.log(response);
+  //         }
+  //       );
+  //   }
+  // }
+   
 }
