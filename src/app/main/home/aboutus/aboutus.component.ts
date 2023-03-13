5b224d9e11d3c943f7a9717bc5eb83c769dca729
Luis Tolentino <luist@4waystech.com>
2023-03-13 09:31:46 -0500

    Actualización de estilos

--- src/app/main/home/aboutus/aboutus.component.ts
+++ src/app/main/home/aboutus/aboutus.component.ts
@@ -3,14 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-aboutus',
   templateUrl: './aboutus.component.html',
-  styleUrls: ['./aboutus.component.css']
+  styleUrls: ['./aboutus.component.css'],
 })
 export class AboutusComponent implements OnInit {
   ngOnInit(): void {
-    let box = document.getElementById("temp");
+    let box = document.getElementById('BannerText');
     setTimeout(() => {
-      box!.style.opacity = "1";
-      box!.style.transition = "3s";
+      box!.style.opacity = '1';
+      box!.style.transition = '3s';
     }, 1000);
   }
 }
