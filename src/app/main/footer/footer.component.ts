5b0b24641ca1c73dc5b98f8a8d3e915bc2b1bd38
jair arroyo <jairarroyo10@hotmail.com>
2023-04-17 11:25:18 -0500

    add links

--- src/app/main/footer/footer.component.ts
+++ src/app/main/footer/footer.component.ts
@@ -4,15 +4,16 @@ import { Constants } from '../../../utils/constants';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
-  facebook : string = Constants.facebook;
-  instagram : string = Constants.instagram;
-  linkd : string = Constants.linkd;
-  footerLogoWhite : string = Constants.footerLogoWhite;
-  twitter : string = Constants.twitter;
-  urlFacebook : string = Constants.urlFacebook;
-  urlInstagram : string = Constants.urlInstagram;
-  mailEtereo : string = Constants.mailEtereo;
+  facebook: string = Constants.facebook;
+  instagram: string = Constants.instagram;
+  linkd: string = Constants.linkd;
+  footerLogoWhite: string = Constants.footerLogoWhite;
+  twitter: string = Constants.twitter;
+  urlFacebook: string = Constants.urlFacebook;
+  urlInstagram: string = Constants.urlInstagram;
+  urlWhatsapp: string = Constants.urlWhatsapp;
+  mailEtereo: string = Constants.mailEtereo;
 }
